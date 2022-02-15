1c387b456176e302d1a0a208d62a09cdfcf78a16
dvthnight <davide.lanotte@protonmail.com>
2022-02-15 18:40:35 +0100

    in base alla scelta mi dice dimensioni griglia

--- app.js
+++ app.js
@@ -5,4 +5,27 @@ const difficoltà = document.getElementById("difficoltà");
 buttonPlay.addEventListener("click",function(){
     const scelta = difficoltà.value;
     console.log(scelta);
+
+    let colonna;
+    let riga;
+
+    switch (scelta) {
+        case "1":
+            riga = 10;
+            colonna = 10;
+            break;
+    
+        case "2":
+            riga = 9;
+            colonna = 9;
+            break;
+
+        case "3":
+            riga = 7;
+            colonna = 7;
+    }
+
+    let griglia = colonna * riga;
+
+    console.log(griglia);
 })
